d68340bb6b965592853a3f38167d28540c34f3ec
Nataliia Horb <nightangnata@gmail.com>
2023-05-10 23:47:56 +0200

    update AсcountRepository

--- src/main/java/com/project/bankproj/repository/AccountRepository.java
+++ src/main/java/com/project/bankproj/repository/AccountRepository.java
@@ -3,6 +3,7 @@ package com.project.bankproj.repository;
 import com.project.bankproj.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,5 +14,5 @@ public interface AccountRepository extends JpaRepository<Account, UUID> {
     Optional<Account> findAccountById(UUID id);
 
     @Query("SELECT a, b FROM Account a JOIN a.agreements b WHERE b.product.id=:id ")
-    List<Account> findAccountsWhereProductId(int id);
+    List<Account> findAccountsWhereProductId(@Param("id") int id);
 }
